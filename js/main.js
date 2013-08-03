ee37e2ad086607fdd9500b0ebe58e40cdd6d88c9
Matej Zilak <teo@teo.sk>
2013-08-03 18:40:02 +0200

    added basic fetching of data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+var module = angular.module('ghdisco', []);
 
+function discoController($scope, $http) {
+    $scope.days = [];
+
+    $scope.loadContrib = function() {
+        $http.jsonp('https://github.com/users/teo-sk/contributions_calendar_data?callback=JSON_CALLBACK', 
+            {headers: {'Accept': 'application/json'}}).success(function(data){
+            console.log(data);
+        });
+    }
+
+}
